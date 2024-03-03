b50a0d953fefab8f13d7baecf4116741c500e800
aheath <aaron.heath5447@gmail.com>
2024-03-03 16:06:30 -0500

    Remove import for vite.svg

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import { Outlet } from 'react-router-dom'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import PageHeader from './components/pageheader'
 
