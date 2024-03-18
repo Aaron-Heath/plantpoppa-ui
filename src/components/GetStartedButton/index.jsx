8a536909aa0f885f7a45ac38c1b8cf3cdcebbeb0
aheath <aaron.heath5447@gmail.com>
2024-03-18 19:07:55 -0400

    Change title. Add content and style to landing page

---
+++ src/components/GetStartedButton/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './style.css';
+
+export default function GetStartedButton() {
+  return (
+    <div className='get-started bold offwhite'>
+    Get started. It's FREE
+    </div>
+  )
+}
