f7ca432c0bac75a6568d439b9416c7ea404c575e
aheath <aaron.heath5447@gmail.com>
2024-03-18 19:16:12 -0400

    Add onclick to get started button

--- src/components/GetStartedButton/index.jsx
+++ src/components/GetStartedButton/index.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 import './style.css';
 
 export default function GetStartedButton() {
+    const navigate = useNavigate();
+
+const handleClick = (e) => {
+    navigate('/login');
+    return;
+}
   return (
-    <div className='get-started bold offwhite'>
+    <div className='get-started bold offwhite' onClick={handleClick}>
     Get started. It's FREE
     </div>
   )
