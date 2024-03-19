7a9a4ea0c2ce20576bb477bb730b26f00cddd02e
aheath <aaron.heath5447@gmail.com>
2024-03-18 21:09:36 -0400

    Add and style landing page header with login button

---
+++ src/components/pages/home/components/LandingHeader/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './style.css'
+import LoginButton from '../../../../LoginButton'
+
+export default function LandingHeader() {
+  return (
+    <header className='landing-header navbar'>
+        <LoginButton/>
+    </header>
+  )
+}
--- src/components/pages/home/index.jsx
+++ src/components/pages/home/index.jsx
@@ -1,22 +1,26 @@
 import GetStartedButton from '../../GetStartedButton'
+import LandingHeader from './components/LandingHeader'
 import './home.css'
 
 export default function Home() {
   return (
-    <div id="landing">
-      <h1><span className='logo'>PlantPoppa</span></h1>
-      <h3>Your plant care co-parent</h3>
-      <div className='offwhite landing-text'>
-        <span class="bold">Plant parenthood made easy.</span>
-        <p> Never forget to water your plants again.</p>
+    <main>
+    <LandingHeader/>
+      <div id="landing">
+        <h1><span className='logo'>PlantPoppa</span></h1>
+        <h3>Your plant care co-parent</h3>
+        <div className='offwhite landing-text'>
+          <span class="bold">Plant parenthood made easy.</span>
+          <p> Never forget to water your plants again.</p>
+        </div>
+        <div id='button-div'>
+        <GetStartedButton/>
+        </div>
+        <div id='login-referral'>
+          <p className='offwhite bold'>Already have an account?</p>
+          <a className='offwhite' href='/login'>Log In Here</a>
+        </div>
       </div>
-      <div id='button-div'>
-      <GetStartedButton/>
-      </div>
-      <div id='login-referral'>
-        <p className='offwhite bold'>Already have an account?</p>
-        <a className='offwhite' href='/login'>Log In Here</a>
-      </div>
-    </div>
+    </main>
   )
 }
