ff2ec848145b40685b9a7b527f701a19c148dd12
aheath <aaron.heath5447@gmail.com>
2024-03-21 18:46:28 -0400

    Remove comments

--- src/components/pages/login/index.jsx
+++ src/components/pages/login/index.jsx
@@ -1,17 +1,13 @@
 import React from 'react'
 import LoginForm from './LoginForm'
-import LandingFooter from '../../LandingFooter'
-import LandingHeader from '../../LandingHeader'
-import PageHeader from '../../pageheader'
+import './style.css'
 
 export default function index() {
   return (
     <>
-    <PageHeader/>
     <div className='d-flex justify-content-center'>
         <LoginForm/>
     </div>
-    <LandingFooter/>
     </>
   )
 }
