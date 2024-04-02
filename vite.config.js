9488f7ea3fa50e4d3e8cdd564c2cbe863b780024
aheath <aaron.heath5447@gmail.com>
2024-04-02 18:56:42 -0400

    install concurrently. Add script to run java jar & react app. Add .env.

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api' : {
-        target: process.env.AUTH_API || "http://localhost:8080",
+        target: 'https://plantpoppa-auth-render.onrender.com',
         changeOrigin: true,
       }
     }
