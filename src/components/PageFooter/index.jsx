567de9f288fa61d8e645f2c08b1d1ba4e73ed367
aheath <aaron.heath5447@gmail.com>
2024-07-13 09:57:51 -0400

    Add home navigation to footer

--- src/components/PageFooter/index.jsx
+++ src/components/PageFooter/index.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import './style.css'
 import AddPlantButton from './components/AddPlantButton'
+import { useNavigate } from 'react-router-dom'
 
 export default function PageFooter() {
+const navigate = useNavigate();
+
+const handleHomeClick = () => {
+  navigate('/app');
+}
+
   return (
     <div id="page-footer">
-      <div className='testo'>TEST</div>
+      <div className='testo' onClick={handleHomeClick}>Home</div>
       <AddPlantButton/>
       <div className='testo'>TEST</div>
     </div>
