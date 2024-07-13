660608f94ed8991ae3ec46aae6ab31a1264517d0
aheath <aaron.heath5447@gmail.com>
2024-07-13 09:49:33 -0400

    Data queries and renders for userPlantPage

--- src/components/pages/application/userplantpage/index.jsx
+++ src/components/pages/application/userplantpage/index.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
+import { useQuery } from 'react-query';
 import { useParams } from 'react-router-dom'
+import { GET_USER_PLANT } from '../../../../schemas/api-requests';
 
 export default function UserPlantPage({...props}) {
     const { uuid } = useParams();
+    const {data, status } = useQuery("userPlant", () => {return GET_USER_PLANT(uuid)});
+
   return (
-    <div>
-        This is the UserPlantPage for {uuid}
+    <div className='plants-card justify-start scroll'>
+      {data ?  <>
+              <h2>{data.nickname || data.plant.common_name}</h2>
+              <div>
+                <p>{data.plant.common_name}</p>
+                <p><i>{data.plant.scientific_name}</i></p>
+              </div>
+              </>
+              :
+              <h2>Loading</h2>
+      }
+
+        
     </div>
   )
 }
