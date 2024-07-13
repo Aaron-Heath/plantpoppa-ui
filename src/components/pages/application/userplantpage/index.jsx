5c737c97160650b61e34a19e5a120068b11c0772
aheath <aaron.heath5447@gmail.com>
2024-07-13 10:27:59 -0400

    style plants card add margin. Render basic plant info on user-plant page

--- src/components/pages/application/userplantpage/index.jsx
+++ src/components/pages/application/userplantpage/index.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useQuery } from 'react-query';
 import { useParams } from 'react-router-dom'
 import { GET_USER_PLANT } from '../../../../schemas/api-requests';
+import './style.css'
 
 export default function UserPlantPage({...props}) {
     const { uuid } = useParams();
@@ -10,11 +11,21 @@ export default function UserPlantPage({...props}) {
   return (
     <div className='plants-card justify-start scroll'>
       {data ?  <>
-              <h2>{data.nickname || data.plant.common_name}</h2>
-              <div>
-                <p>{data.plant.common_name}</p>
-                <p><i>{data.plant.scientific_name}</i></p>
-              </div>
+              <h2 className='display-name'>{data.nickname || data.plant.common_name}</h2>
+              <p className='science-name'><i>{data.plant.scientific_name}</i>
+              </p>
+                <div className='card-row'>
+                  <p><b>Common Name:</b> {data.plant.common_name}</p>
+                </div>
+                <div className='card-row'>
+                  <p><b>Sunlight:</b> {data.plant.sunlight}</p>
+                  <p>{data.plant.sunlight_info}</p>
+                </div>
+                <div className='card-row'>
+                  <p><b>Watering:</b> Water every {data.plant.water_frequency} days</p>
+                  <p>{data.plant.water_info}</p>
+                </div>
+                
               </>
               :
               <h2>Loading</h2>
