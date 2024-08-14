1ddf4aeac6615feb229f25eb152965c51dce6940
aheath <aaron.heath5447@gmail.com>
2024-08-14 08:34:55 -0400

    Should have been in the last commit. CustomButton Class

--- src/components/LandingHeader/index.jsx
+++ src/components/LandingHeader/index.jsx
@@ -1,11 +1,37 @@
 import React from 'react'
 import './style.css'
-import LoginButton from '../LoginButton'
+import CustomButton from '../CustomButton'
+import Auth from '../../utils/auth'
+import { useNavigate } from 'react-router-dom'
 
 export default function LandingHeader() {
+  const navigate = useNavigate();
+
+  const handleLoginClick = (e) => {
+      navigate('login');
+  }
+
+  const handleLogoutClick = (e) => {
+    Auth.logout();
+    navigate('/login');
+  }
+
+  const buttonClassName = 'custom-button button-accent bold'
+  // Button functions as login/logout depending on the Auth.loggedIn() status.
+  const buttonProps = Auth.loggedIn()?
+  {
+    text: "Logout",
+    onClick: handleLogoutClick,
+    className: buttonClassName
+  } :
+  {
+    text:"Login",
+    onClick: handleLoginClick,
+    className: buttonClassName
+  }
   return (
     <header className='landing-header navbar'>
-        <LoginButton/>
+        <CustomButton {...buttonProps}/>
     </header>
   )
 }
